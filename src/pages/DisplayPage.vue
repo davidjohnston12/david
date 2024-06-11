580079d6b65cfd0dc90aa196e65840455d43dab0
davidjohnston12 <djjohnston2007@gmail.com>
2024-06-11 14:15:27 +0100

    task 1 complete

---
+++ src/pages/DisplayPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page class="flex flex-center q-pa-md">
+    <div class="column items-center q-gutter-md">
+      <q-btn label="Back" @click="goBack" />
+      <iframe
+        v-if="url"
+        :src="url"
+        class="iframe-width"
+        frameborder="0"
+      ></iframe>
+      <div v-else>No URL provided</div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const url = ref(localStorage.getItem("submittedUrl") || "");
+
+const goBack = () => {
+  // Navigate back to the previous page
+  router.go(-1);
+};
+</script>
+
+<style>
+.iframe-width {
+  width: 100%;
+  height: 80vh;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { route } from "quasar/wrappers";
+import {
+  createRouter,
+  createMemoryHistory,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
+import routes from "./routes";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+export default route(function () {
+  const createHistory = process.env.SERVER
+    ? createMemoryHistory
+    : process.env.VUE_ROUTER_MODE === "history"
+    ? createWebHistory
+    : createWebHashHistory;
 
-export default router
+  const Router = createRouter({
+    scrollBehavior: () => ({ left: 0, top: 0 }),
+    routes,
+    history: createHistory(process.env.VUE_ROUTER_BASE),
+  });
+
+  return Router;
+});
